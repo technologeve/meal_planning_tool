6e6fec92e2e6766af910432da49664281ba2ec7a
“technologeve” <eve.nicole@icloud.com>
2024-10-08 11:30:43 +0100

    Create procedure to get nutritional info for a meal

--- getRecipe.sql
+++ getRecipe.sql
@@ -2,10 +2,10 @@
 
 USE meal_planning;
 
-drop procedure getRecipe;
+drop procedure get_recipe;
 Delimiter //
 /**
- * getRecipe
+ * get_recipe
  * 
  * Given a meal name, returns a grid of the ingredients needed for 1 serving.
  * 
@@ -13,9 +13,9 @@ Delimiter //
  *   mealName, VARCHAR(100): Name of the meal for which you want the
  * 							 recipe, as appears in 'meals' table
  */
-create procedure getRecipe (mealName VARCHAR(100))
-BEGIN    
-    select ri.amount, i.measurement_unit, i.ingredient_name  
+create procedure get_recipe (mealName VARCHAR(100), keepId BOOL)
+BEGIN
+    select ri.amount, i.measurement_unit, i.ingredient_name
 	from recipe_ingredient ri 
 	join ingredients i 
     -- Join the ingredient measurements with their names
@@ -28,5 +28,53 @@ END//
 DELIMITER ;
 
 -- example usage --
-CALL getRecipe("Chicken fajitas");
+CALL get_recipe("Chicken fajitas");
 
+
+-- get the nutritional values for a meal
+drop procedure get_recipe;
+Delimiter //
+/**
+ * get_nutrition_for_meal
+ * 
+ * Given a meal name, returns the nutritional info for 1 serving.
+ * 
+ * Parameters:
+ *   mealName, VARCHAR(100): Name of the meal for which you want the
+ * 							 nutritional info, as appears in 'meals' table
+ */
+create procedure get_nutrition_for_meal(mealName VARCHAR(100))
+BEGIN
+	-- filter to just nutrition info
+	select 
+		   -- calculate total nutrition for meal
+           sum(nutrition_measurements.nutrition_for_recipe) as nutrition_amt, 
+		   rn.nutrition_units,
+		   rn.nutrition_name,
+           -- compare total meal nutrition to daily reference values
+		   round(100 *sum(nutrition_measurements.nutrition_for_recipe) / rn.daily_nutrition, 2) as percentage_of_daily_ref
+		   
+	-- join nutrition info and recipe
+    from reference_nutrition rn 
+	join 
+		(select joined_table.ingredient_name, joined_table.ingredient_id, i_n.nutrition_id, round(i_n.amount * joined_table.amount, 2) as nutrition_for_recipe
+		from ingredient_nutrition i_n
+		join (select ri.amount, i.measurement_unit, i.ingredient_name, i.ingredient_id  
+			from recipe_ingredient ri 
+			join ingredients i 
+			-- Join the ingredient measurements with their names
+			where i.ingredient_id = ri.ingredient_id and 
+			-- filter to only the requested meal
+			ri.meal_id = 
+			(select m.meal_id from meals m where m.meal_name = mealName)) joined_table
+		where joined_table.ingredient_id = i_n.ingredient_id) 
+	nutrition_measurements
+	where rn.nutrition_id = nutrition_measurements.nutrition_id
+    
+    -- tally all the ingredients in a meal together by nutrition count
+	group by rn.nutrition_name, rn.daily_nutrition, rn.nutrition_units;
+END//
+DELIMITER ;
+
+-- example usage --
+CALL get_nutrition_for_meal("Chicken fajitas");
